b516726ee62556ca4bcb0f96e2e8948b1c4155c1
xumc <yzrxmc1314@gmail.com>
2024-10-30 23:12:24 +0800

    shell template vaList

--- depends/debug.cpp
+++ depends/debug.cpp
@@ -4,7 +4,12 @@
 
 #include "exception.h"
 #include "mutex"
-#define EASY_DEBUG(problem)
+#include "stdarg.h"
+#define EASY_DEBUG(problem)\
+do{\
+    \
+}while(0)
+
 static cycle<problem> st_problems(128);
 static __thread problem *st_problem = nullptr;
 static std::mutex st_mutex;
@@ -22,11 +27,27 @@ void debug_and_reset_problem()
     }
 
 }
-problem & newProblem(t_where where ,pT_char format ...)
+#define STAT_PARSE(format)\
+do{\
+    va_list va_arg;       \
+    va_start(va_arg,format); \
+    snprintf(STAT_BUF,100,format,va_arg); \
+    va_end(va_arg);\
+}while(0)
+#define STAT_PARSE2(format,...)\
+do{\
+    snprintf(STAT_BUF,100,format,##__VA_ARGS__); \
+}while(0)
+problem & newProblem(t_where where ,pT_char format ,...)
 {
+    char STAT_BUF[100] = {0};
+
     debug_and_reset_problem();
+    STAT_PARSE(format);
+    STAT_PARSE2(format);
     std::lock_guard<std::mutex> lockGuard(st_mutex);
     problem & _problem = st_problems.next_step();
+    _problem.init();
     st_problem = &_problem;
     return _problem;
 }
